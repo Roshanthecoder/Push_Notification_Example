2b3c7fe8fece386a5efc64db8364b998a4eadc41
roshan kumar <roshan.kumar@dev.prologicwebsolutions.com>
2023-05-12 12:29:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pushnotification/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pushnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
